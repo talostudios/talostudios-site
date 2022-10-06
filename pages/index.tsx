badd70fee49d25d45d5c350a0a50a077b749dd75
Jes Ugalde <jes@jesia.co>
2022-10-06 02:03:33 -0400

    update layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,14 @@ import type { GetStaticProps, NextPage } from 'next'
 export default function Home() {
   return (    
     <div className='h-screen w-screen'>
-      <div className='flex flex-col justify-center'>
+      <div className='flex flex-col justify-center text-center'>
         <div> 
-          <h1>Talo Studios</h1>
+          <h1>Area 0 </h1>
         </div>
-        <div className='flex flex-row justify-center'>
-          <div className='text-left text-2xl'> Button 1 </div>
-          <div className='border rounded p-8'> Game </div>
-          <div className='text-right text-2xl'> Button 2 </div>
+        <div className='flex flex-row'>
+          <div className='w-1/4 border text-2xl'> Area 1 </div>
+          <div className='w-1/2 border text-2xl'> Area 2 </div>
+          <div className='w-1/4 border text-2xl'> Area 3 </div>
         </div>
       </div>
     </div>

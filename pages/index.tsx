83403ed54b4c374810385fb93bdaadf24dbd2f58
Jes Ugalde <jes@jesia.co>
2022-10-06 23:24:01 -0400

    updated layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,19 @@
 import type { GetStaticProps, NextPage } from 'next'
 
 export default function Home() {
-  return (    
-    <div className='h-screen w-screen'>
-      <div className='flex flex-col justify-center text-center'>
-        <div> 
-          <h1>Area 0 </h1>
-        </div>
-        <div className='flex flex-row'>
-          <div className='w-1/4 border text-2xl'> Area 1 </div>
-          <div className='game-container w-1/2 border text-2xl'>
-            <canvas className="game-canvas border">
-
-            </canvas>
-          </div>
-          <div className='w-1/4 border text-2xl'> Area 3 </div>
-        </div>
+  return (
+    <div className='h-screen w-screen flex flex-col text-center'>
+      <div className='h-1/4 border text-2xl'>
+        <h1>Area 0 </h1>
+      </div>
+      <div className='p-2' />
+      <div className='h-full flex flex-row'>
+        <div className='w-1/4 border text-2xl'> Area 1 </div>
+        <div className='w-1/2 border text-2xl'> Area 2</div>
+        <div className='w-1/4 border text-2xl'> Area 3 </div>
+      </div>
+      <div className='h-1/4 border text-2xl'>
+        <h1>Area 4 </h1>
       </div>
     </div>
   )

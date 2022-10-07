61d900e4abffa52fe18cc18ecac297ea642d0ddf
Jes Ugalde <jes@jesia.co>
2022-10-07 00:24:18 -0400

    center components and add art button

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,21 @@ export default function Home() {
   return (
     <div className='h-screen w-screen flex flex-col text-center'>
       <div className='h-1/4 border text-2xl'>
-        <h1>Area 0 </h1>
+        <h1>Area 0 - Header </h1>
       </div>
       <div className='p-2' />
-      <div className='h-full flex flex-row'>
-        <div className='w-1/4 border text-2xl'> Area 1 </div>
-        <div className='w-1/2 border text-2xl'> Area 2</div>
-        <div className='w-1/4 border text-2xl'> Area 3 </div>
+      <div className='h-full flex'>
+        <div className='w-1/4 border flex items-center justify-center'> 
+          <button className='px-4 py-2 font-medium tracking-wide bg-indigo-300 text-indgo-300'> 
+            Art
+          </button>
+        </div>
+        <div className='w-1/2 border flex items-center justify-center'> Area 2 - Game </div>
+        <div className='w-1/4 border flex items-center justify-center'> Area 3 - Right Button </div>
       </div>
-      <div className='h-1/4 border text-2xl'>
-        <h1>Area 4 </h1>
+      <div className='h-1/4 border flex items-center justify-center'>
+        <h1>Area 4 - Botton Button </h1>
       </div>
     </div>
   )
 }
-
-/** 
-export const getStaticProps: GetStaticProps = async () => {
-
-}
-*/
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  purge : [
+    "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
